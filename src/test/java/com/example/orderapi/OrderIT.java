837ca82b9d6e5f14265405d3a929bde5d28ed88d
Matthew Walker <matthew.walker2@sky.uk>
2018-08-11 13:25:23 +0100

    Run integration tests on random port

--- src/test/java/com/example/orderapi/OrderIT.java
+++ src/test/java/com/example/orderapi/OrderIT.java
@@ -4,17 +4,19 @@ import com.example.orderapi.dto.OrderRequestDTO;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrderIT {
 
-    private TestRestTemplate restTemplate = new TestRestTemplate();
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void givenAnOrderRequest_whenOrderIsSubmitted_thenAnOrderReferenceIsReturned() {
@@ -22,9 +24,22 @@ public class OrderIT {
         OrderRequestDTO orderRequest = new OrderRequestDTO(1, 100);
 
         // when the request is submitted
-        Order order = restTemplate.postForObject("http://localhost:8080/orders", orderRequest, Order.class);
+        Order order = restTemplate.postForObject("/orders", orderRequest, Order.class);
 
         // then an order reference is returned
+        Assert.assertEquals(1, order.getCustomerId());
+        Assert.assertEquals(100, order.getQuantity());
         Assert.assertNotNull(order.getOrderReference());
+    }
+
+    @Test
+    public void givenAnOrderRequest_whenAnOrderIsSubmitted_thenACreatedStatusIsReturned() {
+        // given an order request
+        OrderRequestDTO orderRequest = new OrderRequestDTO(1, 100);
+
+        // when the request is submitted
+        ResponseEntity<Order> responseEntity = restTemplate.postForEntity("/orders", orderRequest, Order.class);
+
+        Assert.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
     }
 }
